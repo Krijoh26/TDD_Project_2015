a182b37fe2f94a7284c647b3fecf1da672726a04
Krijoh26 <krijoh26@gmail.com>
2015-11-25 16:41:35 +0100

    Done with DataCollector

--- Prosjektoppgave Programmering 2/Tests/DataCollectorTest.java
+++ Prosjektoppgave Programmering 2/Tests/DataCollectorTest.java
@@ -1,25 +1,33 @@
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.mockito.Mockito.*;
 
 public class DataCollectorTest {
-	DataCollector dataCollector;
-	FileReader fileReader;
+	private DataCollector dataCollector;
+	private FileReader fileReader;
+	private String fullInputString = "03AC0F 1 110101000000110111001101 001000011110011101001111";
+	private String keyId = "03AC0F";
+	private String firstString = "110101000000110111001101";
+	private String secondString = "001000011110011101001111";
+	private String bitwiseAndResult = "000000000000010101001101";
+	private String bitwiseOrResult = "111101011110111111001111";
+
 
 	@Before
 	public void setUp() {
 		fileReader = mock(FileReader.class);
 		dataCollector = new DataCollector(fileReader);
 	}
-	
+
 	@Test
 	public void openFile_filetxt_returnsTrue() {
 		when(fileReader.openFile("file.txt")).thenReturn(true);
 		assertTrue(dataCollector.openFile("file.txt"));
 		verify(fileReader, times(1)).openFile("file.txt");
 	}
-	
+
 	@Test
 	public void readLine_001000011110011101001111_returnsTheSameString() {
 		String expectedString = "001000011110011101001111";
@@ -27,29 +35,58 @@ public class DataCollectorTest {
 		assertEquals(expectedString, dataCollector.readFile("file.txt"));
 		verify(fileReader, times(1)).readLine();
 	}
-	
+
 	@Test
 	public void hasMore_returnsFalse() {
 		when(fileReader.hasMore()).thenReturn(false);
 		assertEquals(false, dataCollector.hasMore());
 		verify(fileReader, times(1)).hasMore();
 	}
+	
+	@Test
+	public void getInformation_keyId_returnsValueFromKey() {
+		dataCollector.saveInformation(fullInputString);
+		assertEquals("1", dataCollector.getInformation(keyId).getOperator());
+		assertEquals(firstString, dataCollector.getInformation(keyId).getFirstString());
+		assertEquals(secondString, dataCollector.getInformation(keyId).getSecondString());
+		assertEquals(bitwiseAndResult, dataCollector.getInformation(keyId).getBitResult());
+		assertEquals(1357, dataCollector.getInformation(keyId).getIntResult());
+	}
+
+	@Test (expected = IllegalArgumentException.class)
+	public void checkArguments_keyId_throwsIllegalArgument() {
+		dataCollector.checkArguments(keyId);
+	}
+	
+	@Test
+	public void checkArguments_string_runsWithoutProblems() {
+		dataCollector.checkArguments(fullInputString);
+	}
+	
+	@Test
+	public void calculateBitwise_And_returnsBitwiseAndResult() {
+		assertEquals(bitwiseAndResult, dataCollector.calculateBitwise("1", firstString, secondString));
+	}
+	
+	@Test
+	public void calculateBitwise_Or_returnsBitwiseOrResult() {
+		assertEquals(bitwiseOrResult, dataCollector.calculateBitwise("2", firstString, secondString));
+	}
+	
+	@Test
+	public void saveInformation_duplicateKeys_returnsValueFromDuplicate() {
+		dataCollector.saveInformation(fullInputString);
+		dataCollector.saveInformation(fullInputString);
+		assertEquals("1", dataCollector.getDuplicates().get(0).getOperator());
+		assertEquals(firstString, dataCollector.getDuplicates().get(0).getFirstString());
+		assertEquals(secondString, dataCollector.getDuplicates().get(0).getSecondString());
+		assertEquals(bitwiseAndResult, dataCollector.getDuplicates().get(0).getBitResult());
+		assertEquals(1357, dataCollector.getDuplicates().get(0).getIntResult());
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void checkOperators_operator3_throwsIllegalArgument() {
+		String string = "03AC0F 3 110101000000110111001101 001000011110011101001111";
+		dataCollector.saveInformation(string);
+	}
 }
-/*Vi trenger en klasse som tar seg av åpning og lesing av en gitt fil, samt at data lagres unna. Den
-første hex sekvensen representerer en måle-id som skal brukes i forbindelse med lagring i en
-passende collection klasse. Dersom en måle-id allerede er lest og lagret, skal den duplikate måle-id
-og måledata lagres i en annen passende collection klasse som en logg (se nærmere beskrivelse
-nedenfor).
-
-Krav, datainnsamlingsklassen:
-
-Når ei linje med måledata leses, skal følgende skje:
--- Måledata id skal brukes som nøkkel for lagring og gjenfinning av bearbeidede måledata i en
-passende Java collection klasse
--- Feil antall argumenter skal kaste en exception
--- Operasjon (bitwise OR eller AND) skal utføres på måledata (bearbeidingen)
--- Originale måledata samt resultat av operasjon (bearbeidede måledata) skal lagres, både som
-bitstrenger og int verdier, og skal kunne gjenfinnes ved hjelp av måledata id
--- Vi skal logge data med duplikate måledata-id, samt måledata med feil bitoperasjon (alt annet
-enn 1 eller 2 er å anse som en feil), dette skjer på enkleste måte ved at hele tekstlinja som er lest
-lagres unna i en liste (en pasende Java Collection klasse)*/
--- Prosjektoppgave Programmering 2/src/DataCollector.java
+++ Prosjektoppgave Programmering 2/src/DataCollector.java
@@ -1,7 +1,16 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class DataCollector {
 
 	private FileReader fileReader;
+	private Map<String, Information> information = new HashMap<>();
+	private Utility utility = new Utility();
+	private List<Information> duplicates = new ArrayList<>();
+	private List<String []> errorLog = new ArrayList<>();
 
 	public DataCollector(FileReader fileReader) {
 		this.fileReader = fileReader;
@@ -17,5 +26,98 @@ public class DataCollector {
 
 	public boolean hasMore() {
 		return fileReader.hasMore();
+	}
+
+	public Information getInformation(String keyId) {
+		return information.get(keyId);
+	}
+
+	public void saveInformation(String informationToSave){	
+		String[] tokens = splitLine(informationToSave);
+
+		checkOperators(tokens);
+		String bitResult = calculateBitwise(tokens[1], tokens[2], tokens[3]);
+
+		if(information.containsKey(tokens[0])) {
+			duplicates.add(newInformation(tokens, bitResult));
+		}
+
+		information.put(tokens[0], newInformation(tokens, bitResult));	
+	}
+
+	private void checkOperators(String [] tokens) {
+		if (!tokens[1].matches("[12]+")) {
+			errorLog.add(tokens);
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private String[] splitLine(String informationToSave) {
+		StringTokenizer tokenizer = new StringTokenizer(informationToSave);
+		String [] tokens = new String[4];
+
+		for (int i = 0; tokenizer.hasMoreElements(); i++)
+			tokens[i] = tokenizer.nextToken();
+		return tokens;
+	} 
+
+	public void checkArguments(String string) {
+		if (string.length() != 58) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	public String calculateBitwise(String operator, String first, String second) {
+		String stringToReturn = "";
+		if(operator.equals("1")) {
+			stringToReturn = utility.bitwiseAnd(first, second);
+		}
+
+		else if(operator.equals("2")) {
+			stringToReturn = utility.bitwiseOr(first, second);
+		}
+		return stringToReturn;
+	}
+
+	private Information newInformation(String[] tokens, String bitResult) {
+		int intResult = utility.bitToInt(bitResult);
+		return new Information(tokens[1], tokens[2], tokens[3], bitResult, intResult);
+	}
+
+	public List<Information> getDuplicates() {
+		return duplicates;
+	}
+
+	class Information {
+		private String operator, firstString, secondString, bitResult;
+		private int intResult;
+
+		public Information(String operator, String firstString, String secondString, String bitResult, int intResult) {
+			this.operator = operator;
+			this.firstString = firstString;
+			this.secondString = secondString;
+			this.bitResult = bitResult;
+			this.intResult = intResult;
+		}
+
+		public String getOperator() {
+			return operator;
+		}
+
+		public String getFirstString() {
+			return firstString;
+		}
+
+		public String getSecondString() {
+			return secondString;
+		}
+
+		public String getBitResult() {
+			return bitResult;
+		}
+
+		public int getIntResult() {
+			return intResult;
+		}
 	}
 }
